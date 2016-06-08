a99eee4964090c124a17ae45dc9b30337abbbc4f
JJeyatharsini <jeyatharsini.11@cse.mrt.ac.lk>
2016-06-08 12:21:34 +0530

    Completed

--- src/jeya/java/tree/Main.java
+++ src/jeya/java/tree/Main.java
@@ -1,10 +1,14 @@
 package jeya.java.tree;
 
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TreeFrame frame = new TreeFrame();
+		frame.setSize(400,600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 }
